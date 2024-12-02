c2ef55f7fdd0a4fd049f001a3687f3ece58becd5
sondre <sondre.eide@gmail.com>
2024-12-02 21:04:53 +0100

    bruteforce shit solution day2 part2

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -11,12 +11,20 @@ def read_lines(filename):
 def convert_to_integers(string : str) -> list[int]:
     return [int(x) for x in string.split(" ")]
 
-def count_safe_reports(reports):
-    return sum(is_safe_report(report) for report in reports)
+def count_safe_reports(reports, safe_tester = is_safe_report):
+    return sum(safe_tester(report) for report in reports)
 
+def brute_force_safe_tester(report):
+    for number_index in range(0, len(report)):
+        report_copy = report.copy()
+        report_copy.pop(number_index)
+        if is_safe_report(report_copy):
+            return True
+    return False
 if __name__ == '__main__':
     line_reader = read_lines("data.txt")
     reports = [convert_to_integers(line) for line in line_reader]
-    print(reports[0])
-    print(is_safe_report(reports[0]))
-    print(count_safe_reports(reports))
+
+
+    safe_reports = count_safe_reports(reports)
+    print(count_safe_reports(reports, brute_force_safe_tester))
