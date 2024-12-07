aba81c390607dbef497b6b984ecb20d28a3ec8b2
sondre <sondre.eide@gmail.com>
2024-12-07 11:15:33 +0100

    day5 part 1 solved

--- 2024/day5/day5.py
+++ 2024/day5/day5.py
@@ -1,9 +1,9 @@
 
+import os
 
 
 
-
-def load_data(filename) -> (list[str], list[str]):
+def load_data(filename) -> tuple[list[str], list[str]]:
    with open(filename) as f:
         data = f.readlines()
         without_line_breaks = list(map(lambda x: x.split('\n')[0], data))
@@ -13,10 +13,61 @@ def load_data(filename) -> (list[str], list[str]):
     
 
 
-def loop_through_rule(rules : str, pages : str)
+def get_rule_dict(rules_list: list[str]) -> dict[str, str]:
+    rules = {}
+    for rule in rules_list:
+        key, value = rule.split("|")
+        if rules.get(key) != None:
+            rule_key = rules.get(key)
+            rules[key] = rule_key + [value]
+        else: rules[key] = [value]
+    return rules
+    
+def break_rule(update, page, rule_dict):
+    
+    page_index = page.index(update)
+    rule_lookup = rule_dict.get(update) if  rule_dict.get(update) != None else []
+    for rule in rule_lookup:
+        rule_index = page.index(rule) if rule in page else 10000
+        if page_index > rule_index:
+            return False
+    return True
+
+def verify_page(page, rule_dict) -> bool:
+
+    return all([break_rule(update, page, rule_dict) for update in page])
+
+
+def verify_pages(pages, rule_dict) -> list[list]:
+
+    return [page for page in pages if verify_page(page, rule_dict)]
+
+
+def sum_correct_pages(correct_pages) -> int:
+    return sum(int(page[len(page)//2]) for page in correct_pages)
+
+
 
 if __name__ == "__main__":
-    rules, pages = load_data("data.txt")
-    pages: str = ",".join(pages)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(current_dir, "test_data.txt")
+    rules, pages = load_data(data_path)
 
-        
+    rule_dict = get_rule_dict(rules)
+    
+    
+    pages = [page.split(",") for page in pages]
+    correct_pages = verify_pages(pages, rule_dict)
+    print(sum_correct_pages(correct_pages))
+
+
+    # Part1
+    data_path = os.path.join(current_dir, "data.txt")
+    rules, pages = load_data(data_path)
+
+    rule_dict = get_rule_dict(rules)
+    
+    
+    pages = [page.split(",") for page in pages]
+    correct_pages = verify_pages(pages, rule_dict)
+    print(sum_correct_pages(correct_pages))
